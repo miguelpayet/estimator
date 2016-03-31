d0ffebc5909330c0b2c4633d7be7d08ba3ede317
miguelpayet <miguelpayet@gmail.com>
2016-03-31 15:14:55 -0500

    activar todas las clases del modelo

--- src/main/java/pe/com/pps/dao/HibernateUtil.java
+++ src/main/java/pe/com/pps/dao/HibernateUtil.java
@@ -4,7 +4,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
-import pe.com.pps.model.TipoDeCambio;
+import pe.com.pps.model.*;
 
 public class HibernateUtil {
 
@@ -13,7 +13,20 @@ public class HibernateUtil {
 
 	static {
 		configuration = new Configuration();
+		configuration.addAnnotatedClass(Categoria.class);
+		configuration.addAnnotatedClass(Complejidad.class);
+		configuration.addAnnotatedClass(Estimacion.class);
+		configuration.addAnnotatedClass(EstimacionFactor.class);
+		configuration.addAnnotatedClass(Factor.class);
+		configuration.addAnnotatedClass(Funcionalidad.class);
+		configuration.addAnnotatedClass(Interfaz.class);
+		configuration.addAnnotatedClass(Plataforma.class);
+		configuration.addAnnotatedClass(PlataformaValor.class);
+		configuration.addAnnotatedClass(Proveedor.class);
+		configuration.addAnnotatedClass(Tarea.class);
 		configuration.addAnnotatedClass(TipoDeCambio.class);
+		configuration.addAnnotatedClass(TipoDeFactor.class);
+		configuration.addAnnotatedClass(TipoDeInterfaz.class);
 		configuration.configure();
 		ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
 		sessionFactory = configuration.buildSessionFactory(serviceRegistry);
