ab1d9a33f2cf6a94b5903ed30b573cde865a13d2
miguelpayet <miguelpayet@gmail.com>
2016-06-10 15:45:09 -0500

    redondeo a dos decimales al calcular días y horas

--- src/main/java/pe/com/pps/model/TareaEsfuerzo.java
+++ src/main/java/pe/com/pps/model/TareaEsfuerzo.java
@@ -3,6 +3,9 @@ package pe.com.pps.model;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
+
+// la tarea de esfuerzo calcula la duración según la cantidad de recursos asignados / lo mismo para las horas pero al revés
+
 @Entity
 @DiscriminatorValue(value = "1")
 public class TareaEsfuerzo extends Tarea {
@@ -10,7 +13,7 @@ public class TareaEsfuerzo extends Tarea {
 	public Double convertirDiasAHoras(TareaCronograma unaTarea) {
 		Double horas = null;
 		if (unaTarea != null) {
-			horas = unaTarea.getDias() * unaTarea.getRecursos() * 8; // todo: horas del proveedor
+			horas = Util.round(unaTarea.getDias() * unaTarea.getRecursos() * 8, 2); // todo: horas del proveedor
 		}
 		return horas;
 	}
@@ -18,7 +21,7 @@ public class TareaEsfuerzo extends Tarea {
 	public Double convertirHorasADias(TareaCronograma unaTarea) {
 		Double dias = null;
 		if (unaTarea != null) {
-			dias = (unaTarea.getHoras() / unaTarea.getRecursos()) / 8; // todo: horas del proveedor
+			dias = Util.round((unaTarea.getHoras() / unaTarea.getRecursos()) / 8, 2); // todo: horas del proveedor
 		}
 		return dias;
 	}
--- src/main/java/pe/com/pps/model/TareaGestion.java
+++ src/main/java/pe/com/pps/model/TareaGestion.java
@@ -3,14 +3,16 @@ package pe.com.pps.model;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
+// la tarea de gestión se calcula asignando un % del recurso según los días u horas recibidos
+
 @Entity
 @DiscriminatorValue(value = "3")
-public class TareaGestion extends Tarea  {
+public class TareaGestion extends Tarea {
 
 	public Double convertirDiasAHoras(TareaCronograma unaTarea) {
 		Double horas = null;
 		if (unaTarea != null) {
-			horas = unaTarea.getDias() * unaTarea.getPorcentaje() * 8; // todo: horas del proveedor
+			horas = Util.round(unaTarea.getDias() * unaTarea.getPorcentaje() * 8, 2); // todo: horas del proveedor
 		}
 		return horas;
 	}
@@ -18,7 +20,7 @@ public class TareaGestion extends Tarea  {
 	public Double convertirHorasADias(TareaCronograma unaTarea) {
 		Double dias = null;
 		if (unaTarea != null) {
-			dias = unaTarea.getHoras() / (unaTarea.getPorcentaje() * 8); // todo: horas del proveedor
+			dias = Util.round(unaTarea.getHoras() / (unaTarea.getPorcentaje() * 8), 2); // todo: horas del proveedor
 		}
 		return dias;
 	}
