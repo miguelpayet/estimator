0740ea77fdae8587285cde0c0e13c19d49f29e10
miguelpayet <miguelpayet@gmail.com>
2016-06-21 12:33:29 -0500

    añadir para leer una lista de parámetros y no de uno en uno

--- src/main/java/pe/com/pps/dao/DaoParametro.java
+++ src/main/java/pe/com/pps/dao/DaoParametro.java
@@ -1,11 +1,18 @@
 package pe.com.pps.dao;
 
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
 import pe.com.pps.model.Parametro;
 import pe.com.pps.model.ParametroPK;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class DaoParametro extends DaoPK<Parametro, ParametroPK> {
 
 	private static final String PARAMETRO_DESVIACION = "Desviación";
+	private static final String PARAMETRO_EDS = "EDS";
 
 	public DaoParametro() {
 		super(Parametro.class);
@@ -14,6 +21,14 @@ public class DaoParametro extends DaoPK<Parametro, ParametroPK> {
 	public Parametro getDesviacion() {
 		ParametroPK pk = new ParametroPK(PARAMETRO_DESVIACION, 1);
 		return get(pk);
+	}
+
+	public List<String> getNombreEds() {
+		Criteria crit = crearCriteria();
+		crit.add(Restrictions.eq("tipo", PARAMETRO_EDS));
+		crit.addOrder(Order.asc("codigo"));
+		List<Parametro> params = crit.list();
+		return params.stream().map(Parametro::getValor).collect(Collectors.toList());
 	}
 
 }
