8597a8d58a6d26d7440bba10a41fc60ed9b5bfa7
miguelpayet <miguelpayet@gmail.com>
2016-07-05 16:53:53 -0500

    cambios de modelo

--- sql/cambios.sql
+++ sql/cambios.sql
@@ -1,5 +1,5 @@
 -- MySQL Workbench Synchronization
--- Generated: 2016-07-04 13:53
+-- Generated: 2016-07-05 16:45
 -- Model: New Model
 -- Version: 1.0
 -- Project: Name of the project
@@ -9,10 +9,16 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-ALTER TABLE `mydb`.`costo_adicional`
-CHANGE COLUMN `numcosto` `idcosto` INT(11) NOT NULL AUTO_INCREMENT FIRST,
-DROP PRIMARY KEY,
-ADD PRIMARY KEY (`idcosto`);
+ALTER TABLE `mydb`.`estimacion`
+ADD COLUMN `idusuario` INT(11) NOT NULL AFTER `version`,
+ADD INDEX `fk_estimacion_usuario1_idx` (`idusuario` ASC);
+
+ALTER TABLE `mydb`.`estimacion`
+ADD CONSTRAINT `fk_estimacion_usuario1`
+  FOREIGN KEY (`idusuario`)
+  REFERENCES `mydb`.`usuario` (`idusuario`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
