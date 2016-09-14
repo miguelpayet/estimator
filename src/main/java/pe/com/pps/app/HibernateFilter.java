801a017dc9292da568b4af581754cb2ee5e06341
miguelpayet <miguelpayet@gmail.com>
2016-09-14 11:36:31 -0500

    agregar comentarios

--- src/main/java/pe/com/pps/app/HibernateFilter.java
+++ src/main/java/pe/com/pps/app/HibernateFilter.java
@@ -10,24 +10,41 @@ import pe.trazos.dao.HibernateUtil;
 import javax.servlet.*;
 import java.io.IOException;
 
+/**
+ * filtro para configurar la transacción de hibernate en cada request
+ * crea la transacción al inicio del request
+ * hace commit y destruye la transacción al final del request
+ */
 public class HibernateFilter implements Filter {
 
 	private static final Logger log = LoggerFactory.getLogger(HibernateFilter.class);
 
+	/**
+	 * destruye el filtro
+	 */
 	@Override
 	public void destroy() {
-		log.debug("destroy");
+		log.trace("destroy");
 	}
 
+	/**
+	 * crea la transacción, ejecuta el resto de la cadena de filtros y cuando vuelve hace commit a la transacción
+	 *
+	 * @param request  -> el request que se está procesando
+	 * @param response -> el response que se va a enviar
+	 * @param chain    -> cadena de filtros en ejecución
+	 * @throws IOException      -> excepción de input/output
+	 * @throws ServletException -> excepción de tipo servlet
+	 */
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		SessionFactory sf = HibernateUtil.getSessionFactory();
 		Session sesion = sf.getCurrentSession();
 		try {
-			log.debug("doFilter.beginTransaction");
+			log.trace("doFilter.beginTransaction");
 			sesion.beginTransaction();
 			chain.doFilter(request, response);
-			log.debug("doFilter.commit");
+			log.trace("doFilter.commit");
 			sesion.getTransaction().commit();
 		} catch (Throwable ex) {
 			try {
@@ -43,9 +60,10 @@ public class HibernateFilter implements Filter {
 
 	}
 
+	// inicializa el filtro
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
-		log.info("init");
+		log.trace("init");
 	}
 
 }
