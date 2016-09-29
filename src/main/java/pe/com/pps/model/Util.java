f5b4561f38f8c18b2f3539a8971c91e2cb12b11a
miguelpayet <miguelpayet@gmail.com>
2016-09-29 13:34:11 -0500

    funciones para formatear fecha y double

--- src/main/java/pe/com/pps/model/Util.java
+++ src/main/java/pe/com/pps/model/Util.java
@@ -1,10 +1,31 @@
 package pe.com.pps.model;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Util {
 
+	private static DecimalFormat df;
+	private static final SimpleDateFormat sdf = new SimpleDateFormat("dd-MMM-yyyy' 'HH:mm:ss");
+
+	static {
+		//Locale bLocale = new Locale.Builder().setLanguage("en").setRegion("US").build();
+		DecimalFormatSymbols unusualSymbols = new DecimalFormatSymbols(); //bLocale);
+		df = new DecimalFormat("###,###,###.00", unusualSymbols);
+	}
+
+	public static String format(double unDoble) {
+		return df.format(unDoble);
+	}
+
+	public static String format(Date unaFecha) {
+		return sdf.format(unaFecha);
+	}
+
 	public static double round(double unDoble, int unaPrecision) {
-		double test =  Math.round(unDoble * Math.pow(10, unaPrecision)) /  Math.pow(10, unaPrecision);
-		return test;
+		return Math.round(unDoble * Math.pow(10, unaPrecision)) / Math.pow(10, unaPrecision);
 	}
 
 }
