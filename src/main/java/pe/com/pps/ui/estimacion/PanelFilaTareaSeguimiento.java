8f50dc80fd086f3f7a9013cbfd9d6ab9f6af4482
miguelpayet <miguelpayet@gmail.com>
2016-11-30 11:04:29 -0500

    nueva clase

---
+++ src/main/java/pe/com/pps/ui/estimacion/PanelFilaTareaSeguimiento.java
@@ -0,0 +1,12 @@
+package pe.com.pps.ui.estimacion;
+
+import org.apache.wicket.model.IModel;
+import pe.com.pps.model.TareaCronograma;
+
+public class PanelFilaTareaSeguimiento extends PanelFilaTareaFija {
+
+	public PanelFilaTareaSeguimiento(String id, IModel<TareaCronograma> model) {
+		super(id, model);
+	}
+
+}
