c0c92fab6b1d4d81db4379eee42db0fce55a6d13
miguelpayet <miguelpayet@gmail.com>
2016-12-20 13:52:20 -0500

    nueva clase

---
+++ src/main/java/pe/com/pps/app/EstimadorSessionListener.java
@@ -0,0 +1,22 @@
+package pe.com.pps.app;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.wicket.ISessionListener;
+import org.apache.wicket.Session;
+
+public class EstimadorSessionListener implements ISessionListener {
+
+	private static Logger log = LogManager.getLogger(EstimadorSessionListener.class);
+
+	@Override
+	public void onCreated(Session session) {
+		log.info("nueva sesión web {}", session.getId());
+	}
+
+	@Override
+	public void onUnbound(String sessionId) {
+
+	}
+
+}
