5f7c22017085bc3c4a938e7d8c09805e20a3be2f
miguelpayet <miguelpayet@gmail.com>
2016-12-20 13:53:13 -0500

    homologar log statements

--- src/main/java/pe/com/pps/app/MiRequestListener.java
+++ src/main/java/pe/com/pps/app/MiRequestListener.java
@@ -13,47 +13,47 @@ public class MiRequestListener implements IRequestCycleListener {
 
 	@Override
 	public void onBeginRequest(RequestCycle cycle) {
-		log.debug("MiRequestListener.onBeginRequest {}", cycle.getRequest().getOriginalUrl());
+		log.debug("onBeginRequest {}", cycle.getRequest().getOriginalUrl());
 	}
 
 	@Override
 	public void onDetach(RequestCycle cycle) {
-		log.trace("MiRequestListener.onDetach {}", cycle.getRequest().getOriginalUrl());
+		log.trace("onDetach {}", cycle.getRequest().getOriginalUrl());
 	}
 
 	@Override
 	public void onEndRequest(RequestCycle cycle) {
-		log.debug("MiRequestListener.onEndRequest {}", cycle.getRequest().getOriginalUrl());
+		log.debug("onEndRequest {}", cycle.getRequest().getOriginalUrl());
 	}
 
 	@Override
 	public IRequestHandler onException(RequestCycle cycle, Exception ex) {
-		log.error("MiRequestListener.onException {} -> {}", cycle.getRequest().getOriginalUrl(), ex.getMessage());
+		log.error("onException {} -> {}", cycle.getRequest().getOriginalUrl(), ex.getMessage());
 		return null;
 	}
 
 	@Override
 	public void onExceptionRequestHandlerResolved(RequestCycle cycle, IRequestHandler handler, Exception ex) {
-		log.trace("MiRequestListener.onExceptionRequestHandlerResolved {} -> {}", cycle.getRequest().getOriginalUrl(), ex.getMessage());
+		log.trace("onExceptionRequestHandlerResolved {} -> {}", cycle.getRequest().getOriginalUrl(), ex.getMessage());
 	}
 
 	@Override
 	public void onRequestHandlerExecuted(RequestCycle cycle, IRequestHandler handler) {
-		log.trace("MiRequestListener.onRequestHandlerExecuted {}", cycle.getRequest().getOriginalUrl());
+		log.trace("onRequestHandlerExecuted {}", cycle.getRequest().getOriginalUrl());
 	}
 
 	@Override
 	public void onRequestHandlerResolved(RequestCycle cycle, IRequestHandler handler) {
-		log.trace("MiRequestListener.onRequestHandlerResolved {}", cycle.getRequest().getOriginalUrl());
+		log.trace("onRequestHandlerResolved {}", cycle.getRequest().getOriginalUrl());
 	}
 
 	@Override
 	public void onRequestHandlerScheduled(RequestCycle cycle, IRequestHandler handler) {
-		log.trace("MiRequestListener.onRequestHandlerScheduled {}", cycle.getRequest().getOriginalUrl());
+		log.trace("onRequestHandlerScheduled {}", cycle.getRequest().getOriginalUrl());
 	}
 
 	@Override
 	public void onUrlMapped(RequestCycle cycle, IRequestHandler handler, Url url) {
-		log.trace("MiRequestListener.onUrlMapped {}", cycle.getRequest().getOriginalUrl());
+		log.trace("onUrlMapped {}", cycle.getRequest().getOriginalUrl());
 	}
 }
