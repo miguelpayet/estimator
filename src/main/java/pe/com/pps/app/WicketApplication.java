946acf42205cff3557831da0d82aacfb4550b181
miguelpayet <miguelpayet@gmail.com>
2016-12-20 13:56:24 -0500

    simplificar un poco el código del método init

--- src/main/java/pe/com/pps/app/WicketApplication.java
+++ src/main/java/pe/com/pps/app/WicketApplication.java
@@ -1,11 +1,13 @@
 package pe.com.pps.app;
 
 import de.agilecoders.wicket.core.Bootstrap;
+import org.apache.wicket.Session;
 import org.apache.wicket.authorization.strategies.CompoundAuthorizationStrategy;
 import org.apache.wicket.authroles.authentication.AbstractAuthenticatedWebSession;
 import org.apache.wicket.authroles.authorization.strategies.role.RoleAuthorizationStrategy;
 import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.request.cycle.IRequestCycleListener;
+import org.apache.wicket.request.Request;
+import org.apache.wicket.request.Response;
 import pe.com.pps.auth.PaginaEstimacionAuthorizationStrategy;
 import pe.com.pps.model.*;
 import pe.com.pps.ui.estimacion.PaginaEstimacion;
@@ -15,15 +17,16 @@ import pe.com.pps.ui.login.PaginaLoginEstimator;
 import pe.com.pps.ui.login.PaginaNuevoPasswordEstimator;
 import pe.com.pps.ui.vista.PaginaVistaEstimacion;
 import pe.trazos.dao.HibernateUtil;
+import pe.trazos.login.auth.AuthenticatedWebApplicationBase;
 import pe.trazos.login.auth.LoginRoleCheckingStrategy;
 import pe.trazos.login.auth.LoginSecurityUtil;
 import pe.trazos.login.auth.SesionShiro;
-import pe.trazos.login.auth.AuthenticatedWebApplicationBase;
 
 import java.util.List;
 
 public class WicketApplication extends AuthenticatedWebApplicationBase {
 
+	/*lista de clases de entidad que es necesario registrar con hibernate antes de inicializarlo */
 	static {
 		List<Class> clases = LoginSecurityUtil.getClases();
 		clases.add(Actor.class);
@@ -61,10 +64,16 @@ public class WicketApplication extends AuthenticatedWebApplicationBase {
 		return PaginaListaEstimaciones.class;
 	}
 
+	/**
+	 * @return la página de cambio de password
+	 */
 	public Class<? extends WebPage> getPaginaCambioPassword() {
 		return PaginaCambioPasswordEstimator.class;
 	}
 
+	/**
+	 * @return la página de login
+	 */
 	public Class<? extends WebPage> getPaginaLogin() {
 		return PaginaLoginEstimator.class;
 	}
@@ -92,16 +101,14 @@ public class WicketApplication extends AuthenticatedWebApplicationBase {
 	public void init() {
 		super.init();
 		// hibernate request cycle listener
-		IRequestCycleListener hibernateListener;
-		hibernateListener = new HibernateRequestListener();
-		getRequestCycleListeners().add(hibernateListener);
+		getRequestCycleListeners().add(new HibernateRequestListener());
 		// locale request cycle listener
-		IRequestCycleListener localeListener;
-		localeListener = new LocaleRequestListener();
-		getRequestCycleListeners().add(localeListener);
+		getRequestCycleListeners().add(new LocaleRequestListener());
+		// session listener
+		//getSessionListeners().add(new EstimadorSessionListener());
 		// wicket bootstrap
 		Bootstrap.install(this);
-		// role checking strategy para autorización
+		// estrategias de autorización
 		CompoundAuthorizationStrategy cps = new CompoundAuthorizationStrategy();
 		cps.add(new RoleAuthorizationStrategy(new LoginRoleCheckingStrategy()));
 		cps.add(new PaginaEstimacionAuthorizationStrategy());
