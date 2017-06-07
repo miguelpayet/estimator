0ecb37e13eaaee59a500eb286b270b7429050a18
miguelpayet <miguelpayet@gmail.com>
2017-06-07 16:53:54 -0500

    wip - dao para consultas (semi) complejas

--- src/main/java/pe/com/pps/ui/providers/ProviderEstimacion.java
+++ src/main/java/pe/com/pps/ui/providers/ProviderEstimacion.java
@@ -1,12 +1,12 @@
 package pe.com.pps.ui.providers;
 
-import org.apache.wicket.extensions.markup.html.repeater.data.sort.ISortState;
 import org.apache.wicket.extensions.markup.html.repeater.data.sort.SortOrder;
 import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.Model;
-import org.hibernate.criterion.Order;
-import org.hibernate.criterion.Restrictions;
 import pe.com.pps.model.Estimacion;
+import pe.trazos.dao.query.QueryFiltrado;
+import pe.trazos.dao.query.TipoAgrupacionFiltro;
+import pe.trazos.dao.query.TipoFiltro;
+import pe.trazos.dao.query.FiltroQueryAgrupado;
 
 public class ProviderEstimacion extends ProviderTabla<Estimacion, FiltroTablaNombre> {
 
@@ -17,38 +17,30 @@ public class ProviderEstimacion extends ProviderTabla<Estimacion, FiltroTablaNom
 	}
 
 	@Override
-	public IModel model(Object object) {
-		return new Model<>((Estimacion) object);
-	}
-
-	@Override
-	public void ordenarQuery(QueryCriteria unQuery) {
-		ISortState<String> sort = getSortState();
-		SortOrder order = sort.getPropertySortOrder("numero");
-		if (order.equals(SortOrder.ASCENDING)) {
-			unQuery.getCriteria().addOrder(Order.asc("numero"));
-			unQuery.getCriteria().addOrder(Order.asc("fase"));
-		} else {
-			unQuery.getCriteria().addOrder(Order.desc("numero"));
-			unQuery.getCriteria().addOrder(Order.desc("fase"));
-		}
-		unQuery.getCriteria().addOrder(Order.asc("fase"));
-	}
-
-	@Override
-	protected void setearFiltros(QueryCriteria query) {
+	protected void filtrar(QueryFiltrado query) {
 		String valor = getFilterState().getNombre().trim();
-		Integer valorEntero;
 		try {
-			valorEntero = Integer.valueOf(valor);
+			query.agregarFiltro("numero", Integer.valueOf(valor), TipoFiltro.EQUAL);
 		} catch (NumberFormatException e) {
-			valorEntero = null;
+			FiltroQueryAgrupado filtro = new FiltroQueryAgrupado(TipoAgrupacionFiltro.OR);
+			valor = "%" + valor + "%";
+			filtro.agregar("nombre", valor, TipoFiltro.EQUAL);
+			filtro.agregar("eds", valor, TipoFiltro.EQUAL);
+			query.agregarFiltro(filtro);
 		}
-		if (valorEntero != null) {
-			query.getCriteria().add(Restrictions.eq("numero", valorEntero));
-		} else {
-			query.getCriteria().add(Restrictions.or(Restrictions.like("nombre", "%" + valor + "%"), Restrictions.like("eds", "%" + valor + "%")));
-		}
+	}
+
+	@Override
+	public IModel<Estimacion> model(Estimacion object) {
+		return null;
+	}
+
+	@Override
+	public void ordenarQuery(QueryFiltrado unQuery) {
+		SortOrder orden = getSortState().getPropertySortOrder("numero");
+		unQuery.addOrder("numero", orden);
+		unQuery.addOrder("fase", orden);
+		unQuery.addOrder("fase", SortOrder.ASCENDING);
 	}
 
 }
